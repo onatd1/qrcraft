30c1f1941bb8cac3c74e1ff59ab7d48ab2bf0ca5
onatd1 <didemonat12@gmail.com>
2026-04-19 12:16:09 +0300

    Update QRForm.tsx

--- src/components/Generator/QRForm.tsx
+++ src/components/Generator/QRForm.tsx
@@ -1,11 +1,16 @@
 import { useQRGenerator } from "../../hooks/useQRGenerator";
 
 export default function QRForm() {
-  const { value } = useQRGenerator();
+  const { value, setValue } = useQRGenerator();
 
   return (
     <form>
-      <input type="text" placeholder="Enter QR content" defaultValue={value} />
+      <input
+        type="text"
+        placeholder="Enter QR content"
+        defaultValue={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
     </form>
   );
 }
