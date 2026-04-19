5a9a1f09b8140f91ad11580bfa888d782c2d8e87
onatd1 <didemonat12@gmail.com>
2026-04-19 11:24:27 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,14 @@
+import QRPreview from "../components/Generator/QRPreview";
+import Customizer from "../components/Generator/Customizer";
+import TypeSelector from "../components/Generator/TypeSelector";
+
 export default function Home() {
-  return <div>Welcome to QRCraft</div>;
+  return (
+    <div>
+      <h2>Welcome to QRCraft</h2>
+      <TypeSelector />
+      <Customizer />
+      <QRPreview />
+    </div>
+  );
 }
