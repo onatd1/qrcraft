7df007fad99209e4520b4a83d18ddb7ba988febc
onatd1 <didemonat12@gmail.com>
2026-04-19 11:25:22 +0300

    Update Customizer.tsx

--- src/components/Generator/Customizer.tsx
+++ src/components/Generator/Customizer.tsx
@@ -1,3 +1,13 @@
+import ColorPicker from "./ColorPicker";
+import ShapeSelector from "./ShapeSelector";
+import LogoUpload from "./LogoUpload";
+
 export default function Customizer() {
-  return <div>Customizer Panel</div>;
+  return (
+    <div>
+      <ColorPicker />
+      <ShapeSelector />
+      <LogoUpload />
+    </div>
+  );
 }
