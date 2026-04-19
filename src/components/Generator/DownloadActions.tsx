04efdc5de49d80f4a392c90d4a85fad03b9a5da3
onatd1 <didemonat12@gmail.com>
2026-04-19 11:56:23 +0300

    Update DownloadActions.tsx

--- src/components/Generator/DownloadActions.tsx
+++ src/components/Generator/DownloadActions.tsx
@@ -1,9 +1,11 @@
+import { exportImage } from "../../utils/imageExport";
+
 export default function DownloadActions() {
   return (
     <div>
-      <button>PNG</button>
-      <button>SVG</button>
-      <button>PDF</button>
+      <button onClick={() => exportImage("png")}>PNG</button>
+      <button onClick={() => exportImage("svg")}>SVG</button>
+      <button onClick={() => exportImage("pdf")}>PDF</button>
     </div>
   );
 }
